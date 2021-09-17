8b618495d00896a7bb34c1a7d417d800ad5c5dd9
Hugo Ledoux <h.ledoux@tudelft.nl>
2021-09-17 11:35:54 +0200

    Add more unit tests

---
+++ tests/children_parents_consistency.rs
@@ -0,0 +1,84 @@
+use crate::cjval::CJValidator;
+use cjval;
+use serde_json::json;
+use serde_json::Value;
+
+fn get_data() -> Value {
+    let j_mininal = r#"
+        {
+            "type": "CityJSON",
+            "version": "1.1",
+            "CityObjects":
+            {
+                "LondonTower":
+                {
+                    "type": "Building",
+                    "children":
+                    [
+                        "oneroom"
+                    ]
+                },
+                "oneroom":
+                {
+                    "type": "BuildingRoom",
+                    "parents":
+                    [
+                        "LondonTower"
+                    ]
+                }
+            },
+            "vertices": [],
+            "transform":
+            {
+                "scale":
+                [
+                    0.001,
+                    0.001,
+                    0.001
+                ],
+                "translate":
+                [
+                    0.0,
+                    0.0,
+                    0.0
+                ]
+            }
+        }
+        "#;
+    let v: Value = serde_json::from_str(&j_mininal).unwrap();
+    v
+}
+
+#[test]
+fn valid() {
+    let j = get_data();
+    let v: CJValidator = CJValidator::from_str(&j.to_string()).unwrap();
+    let re = v.validate_schema();
+    assert!(re.is_empty());
+}
+
+#[test]
+fn no_child() {
+    let mut j = get_data();
+    j["CityObjects"]["LondonTower"]["children"]
+        .as_array_mut()
+        .unwrap()
+        .push(json!("hugo"));
+    // println!("=====>{:?}", ar);
+    let v: CJValidator = CJValidator::from_str(&j.to_string()).unwrap();
+    let re = v.parent_children_consistency();
+    assert!(!re.is_empty());
+}
+
+#[test]
+fn no_parent() {
+    let mut j = get_data();
+    j["CityObjects"]["oneroom"]["parents"]
+        .as_array_mut()
+        .unwrap()
+        .push(json!("hugo"));
+    // println!("=====>{:?}", ar);
+    let v: CJValidator = CJValidator::from_str(&j.to_string()).unwrap();
+    let re = v.parent_children_consistency();
+    assert!(!re.is_empty());
+}
--- tests/invalid_json.rs
+++ tests/invalid_json.rs
@@ -1,5 +1,6 @@
 use crate::cjval::CJValidator;
 use cjval;
+use serde_json::json;
 use serde_json::Value;
 
 fn get_minimal() -> Value {
@@ -22,8 +23,69 @@ fn get_minimal() -> Value {
 #[test]
 fn minimal() {
     let j = get_minimal();
-    let mut v: CJValidator = CJValidator::from_str(&j.to_string());
+    let v: CJValidator = CJValidator::from_str(&j.to_string()).unwrap();
     let re = v.validate_schema();
-    println!("{:?}", re);
     assert!(re.is_empty());
+
+    let mut j2 = j.clone();
+    j2.as_object_mut().unwrap().remove("vertices").unwrap();
+    let v2: CJValidator = CJValidator::from_str(&j2.to_string()).unwrap();
+    let re = v2.validate_schema();
+    assert!(!re.is_empty());
+}
+
+#[test]
+fn version() {
+    let j_mininal = r#"
+        {
+          "type": "Potato",
+          "version": "1.1",
+          "transform": {
+            "scale": [0.0, 0.0, 0.0],
+            "translate": [1.0, 1.0, 1.0]
+          },
+          "CityObjects": {},
+          "vertices": []
+        }
+        "#;
+    let mut j: Value = serde_json::from_str(&j_mininal).unwrap();
+    *j.get_mut("version").unwrap() = json!("1.0");
+    let mut v: CJValidator = CJValidator::from_str(&j.to_string()).unwrap();
+    let mut re = v.validate_schema();
+    assert!(!re.is_empty());
+
+    j.as_object_mut().unwrap().remove("version");
+    v = CJValidator::from_str(&j.to_string()).unwrap();
+    re = v.validate_schema();
+    assert!(!re.is_empty());
+}
+
+#[test]
+fn non_cityjson() {
+    let j_mininal = r#"
+        {
+          "type": "Potato",
+          "version": "1.1",
+          "transform": {
+            "scale": [0.0, 0.0, 0.0],
+            "translate": [1.0, 1.0, 1.0]
+          },
+          "CityObjects": {},
+          "vertices": []
+        }
+        "#;
+    let mut j: Value = serde_json::from_str(&j_mininal).unwrap();
+    let mut v: CJValidator = CJValidator::from_str(&j.to_string()).unwrap();
+    let mut re = v.validate_schema();
+    assert!(!re.is_empty());
+
+    // *j.get_mut("type").unwrap() = json!("CityJSON");
+    // v = CJValidator::from_str(&j.to_string());
+    // re = v.validate_schema();
+    // assert!(re.is_empty());
+
+    j.as_object_mut().unwrap().remove("type");
+    v = CJValidator::from_str(&j.to_string()).unwrap();
+    re = v.validate_schema();
+    assert!(!re.is_empty());
 }
